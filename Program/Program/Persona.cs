0e3ebdb5099e46b5ee294c7c880b99172e4b8e16
Christian Carr-Rollitt Cabrera <chrcarcab@alu.edu.gva.es>
2026-09-25 18:47:59 +0200

    Creados setter y getter

--- Program/Program/Persona.cs
+++ Program/Program/Persona.cs
@@ -14,6 +14,17 @@ namespace Program
             Nombre = nombre;
             Edad = edad;
         }
+
+        public int getEdad()
+        {
+            return Edad;
+        }
+
+        public void setEdad(int nuevaEdad)
+        {
+            this.Edad = nuevaEdad;
+        }
+
         public void MostrarDatos()
         {
             Console.WriteLine($"Nombre: {Nombre}, Edad: {Edad} años");
