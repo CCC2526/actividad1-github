5da5d124387d7536698bd63d852bd0e9428920f7
Christian Carr-Rollitt Cabrera <chrcarcab@alu.edu.gva.es>
2026-09-25 18:23:05 +0200

    Creado el constructor de Persona

--- Program/Program/Persona.cs
+++ Program/Program/Persona.cs
@@ -8,5 +8,11 @@ namespace Program
     {
         public string Nombre { get; set; }
         public int Edad { get; set; }
+
+        public Persona(string nombre, int edad)
+        {
+            Nombre = nombre;
+            Edad = edad;
+        }
     }
 }
