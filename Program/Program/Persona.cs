8fa0c896f5edd2965199838ae40187a4cb5e4628
Christian Carr-Rollitt Cabrera <chrcarcab@alu.edu.gva.es>
2026-09-25 18:21:44 +0200

    Creada la clase Persona con atributos básicos

---
+++ Program/Program/Persona.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Program
+{
+    internal class Persona
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+    }
+}
