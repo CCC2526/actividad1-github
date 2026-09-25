509f953a21ae5cd51f5b04d5da6989ce4b72dad8
Christian Carr-Rollitt Cabrera <chrcarcab@alu.edu.gva.es>
2026-09-25 18:51:04 +0200

    Creada llamada a setter y getter en main

--- Program/Program/Program.cs
+++ Program/Program/Program.cs
@@ -5,6 +5,12 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            Persona persona1 = new Persona("Carlos", 25);
+
+            persona1.setEdad(26);
+
+            Console.WriteLine("La nueva edad obtenida con getEdad() es: " + persona1.getEdad());
         }
     }
 }
