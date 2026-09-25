dd1ffb15d16f772c709a94097ae94b5c66323dba
Irene Pamies Fernández <irepamfer@alu.edu.gva.es>
2026-09-25 18:26:50 +0200

    feat: Subtarea 2 - Crear constructor de Producto

--- Program/Program/Producto.cs
+++ Program/Program/Producto.cs
@@ -8,5 +8,11 @@ namespace Program
     {
         public string Nombre { get; set; }
         public double Precio { get; set; }
+
+        public Producto(string nombre, double precio)
+        {
+            Nombre = nombre;
+            Precio = precio;
+        }
     }
 }
