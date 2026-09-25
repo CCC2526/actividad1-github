79efcdcf4f6b1f79bdc6b3889a85fca75ea80338
Irene Pamies Fernández <irepamfer@alu.edu.gva.es>
2026-09-25 18:24:38 +0200

    Creada la clase producto con atributos básicos

--- Program/Program/Producto.cs
+++ Program/Program/Producto.cs
@@ -4,7 +4,9 @@ using System.Text;
 
 namespace Program
 {
-    internal class Producto
+    public class Producto
     {
+        public string Nombre { get; set; }
+        public double Precio { get; set; }
     }
 }
